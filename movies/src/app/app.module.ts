0e86d387d6d48d48b1746eb1b92fb11f5507c49f
praneeth377 <praneethtiger6@gmail.com>
2024-12-03 13:01:40 +0530

    add new pages

--- movies/src/app/app.module.ts
+++ movies/src/app/app.module.ts
@@ -8,13 +8,17 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { CardComponent } from './common/card/card.component';
+import { MovielistComponent } from './pages/movielist/movielist.component';
+import { MoviedetailComponent } from './pages/moviedetail/moviedetail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     HomeComponent,
-    CardComponent
+    CardComponent,
+    MovielistComponent,
+    MoviedetailComponent
   ],
   imports: [
     BrowserModule,
