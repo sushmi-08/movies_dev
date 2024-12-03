d0129adf78d360f95286e05c1b51ebccba960b3e
Mukul Kumar Sahu <MukulKumar.Sahu@triconinfotech.com>
2024-12-03 10:51:05 +0530

    Added Model

---
+++ Backend/models/movie.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const movieSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    category: { type: String, required: true },
+    rating: { type: Number, required: true },
+    cast: { type: [String], required: true },
+    director: { type: String, required: true },
+    release_date: { type: Date, required: true },
+    description: { type: String, required: true },
+    duration: { type: Number, required: true },
+    availability: { type: Boolean, default: true },
+    image_url: { type: String, required: true },
+});
+
+module.exports = mongoose.model('Movie', movieSchema);
